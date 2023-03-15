e6c7868d865e661302b340a8b6cd4edc36d5fda1
madhursingh04 <madhursingh04072@gmail.com>
2023-03-15 15:04:12 +0530

    Home page changed.
    api endpoints are created.
    dockerfile created.
    database connection created.

---
+++ pages/api/form-input.ts
@@ -0,0 +1,20 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req: { method: any; body:any },res: any){
+    try{
+    const client = await clientPromise
+    const db = client.db("database")
+    const {method}=req;
+    if (method === "POST") {
+        const body=JSON.parse(req.body)
+        const post=await db.collection("posts").insertOne(body)    
+        res.status(200).json(post);
+      }
+    if(method==="GET")  {
+        const post=await db.collection("posts").find({}).toArray();
+        res.json({status:200,data:post})
+    }
+    }catch(e){
+        
+    }
+}
